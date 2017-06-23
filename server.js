4193af5bd6bb69f507329cb24219533ec947bc86
Walter <wdacostapaquete@norsys.fr>
2017-06-23 18:36:57 +0200

    init gestion de formulaire post

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 
-
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/', (request, response) => {
-    response.send('hello akkiii');
+/** =========== Moteur de template =========== */
+app.set('view engine', 'ejs')
+
+/** =========== MiddleWare =========== */
+app.use('/assets', express.static('public'))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+
+/** =========== Routes =========== */
+app.get('/', (req, res) => {
+    res.render('pages/index')
 })
 
+app.post('/', (req, res) => {
+    if(req.body.message === undefined || req.body.message ===''){
+        res.render('pages/index', {'error':'Enter a message please !'})
+    }
+})
+
+/** =========== Port =========== */
 app.listen('8080');
