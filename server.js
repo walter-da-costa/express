4a39561cfd6b9d1a1e6e6426248aea1d73a8c865
Walter <wdacostapaquete@norsys.fr>
2017-06-28 17:11:31 +0200

    testing mongo_db connection

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var mongoose = require('mongoose');
+var db = require('./config/db_mongo');
+var moment = require('./config/moment');
 var app = express();
 
 /** =========== Moteur de template =========== */
@@ -26,29 +29,61 @@ app.use(require('./middlewares/flash'))
 app.get('/', (request, response) => {
     var Message = require('./models/message')
     Message.all(function (messages) {
-        response.render('pages/index', {'messages' : messages})
+        //response.render('pages/index', {'messages' : messages})
+        response.render('pages/index', {'messages' : messages, 'moment':moment})
     })
 })
+
+//example with mongo db
+/*app.get('/', (request, response) => {
+
+    var collection = db.get().collection('messages')
+    collection.find().toArray(function(err, messages) {
+
+        response.render('pages/index', {'messages' : messages, 'moment':moment})
+    })
+})*/
 
 app.post('/', (request, response) => {
     if(request.body.message === undefined || request.body.message ===''){
         request.flash('error', 'Il y a une erreur !')
         response.redirect('/');
     }else{
+        //MYSQL
         var Message = require('./models/message')
         Message.create(request.body.message, function () {
             request.flash('success', 'Merci !')
             response.redirect('/');
-        })
+        })//*/
+
+        //MONGO
+        /*var collection = db.get().collection('messages')
+        collection.insertOne( { content: request.body.message, qty: new Date() } );
+        request.flash('success', 'Merci !')
+        response.redirect('/');*/
     }
 })
 
 app.get('/message/:id', (request, response) => {
     var Message = require('./models/message')
     Message.find(request.params.id, function (message) {
-        response.render('pages/message', {'message' : message})
+        response.render('pages/message', {'message' : message })
     })
 })
 
-/** =========== Port =========== */
-app.listen('8080');
+/** =========== Start APP =========== */
+
+app.listen('8088');
+
+//test db connection WiTH MONGO
+/*db.connect('mongodb://localhost:27017/testdb', function(err) {
+    if (err) {
+        console.log('Unable to connect to Mongo.')
+        process.exit(1)
+    } else {
+        app.listen(8088, function() {
+            console.log('Listening on port 8088...')
+        })
+    }
+})*/
+
